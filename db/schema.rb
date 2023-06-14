1a099894fc4c57429d95c719e93895056a826e04
awais <aaawis786786@gmail.com>
2023-06-14 13:17:27 +0500

    Added the render configuration for deployment

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_13_105038) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "post_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
